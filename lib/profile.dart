e49a25867bc20b0c71ed76304bb6f8d74700344a
siewleeon <onsiewlee@gmail.com>
2025-05-07 10:27:34 +0800

    merge all

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:second_hand_shop/accountSetting.dart';
 import './edit_profile.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -18,31 +17,17 @@ class _ProfilePageState extends State<ProfilePage> {
   String phone = '';
   String email = '';
   String address = '';
+  bool isMember = false;
   String avatarUrl = '';
   String sampleAvatarUrl =
       'https://cdn-icons-png.flaticon.com/512/3135/3135715.png';
-
 
   void _navigateToEditPage() async {
     final shouldRefresh = await Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) =>
-            EditProfilePage(userUid: FirebaseAuth.instance.currentUser!.uid),
-      ),
-    );
-
-    if (shouldRefresh == true) {
-      _loadUserProfile();
-    }
-  }
-
-  void _navigateToSettingPage() async {
-    final shouldRefresh = await Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) =>
-            SettingsPage(),
+            EditProfilePage(userId: FirebaseAuth.instance.currentUser!.uid),
       ),
     );
 
@@ -61,18 +46,18 @@ class _ProfilePageState extends State<ProfilePage> {
     final currentUser = FirebaseAuth.instance.currentUser;
     if (currentUser == null) return;
 
-    final UID = currentUser.uid;
+    final userId = currentUser.uid;
 
     DocumentSnapshot doc =
-    await FirebaseFirestore.instance.collection('Users').doc(UID).get();
+    await FirebaseFirestore.instance.collection('Users').doc(userId).get();
     if (doc.exists) {
       Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
       setState(() {
-        userId=data['userId'] ?? '';
         username = data['name'] ?? '';
         phone = data['phone'] ?? '';
         email = data['email'] ?? '';
         address = data['address'] ?? '';
+        isMember = data['member_Status'] ?? false;
         avatarUrl = data['profile_Picture'] ?? '';
       });
     }
@@ -93,13 +78,15 @@ class _ProfilePageState extends State<ProfilePage> {
           ),
           // Foreground content
           SingleChildScrollView(
+
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start, // Aligns the column content to the left
               children: [
                 buildTopSection(),
 
                 buildCardSection("My Points", [
-                  buildFeatureButton(label: "Voucher", icon: Icons.confirmation_number),
-                  buildFeatureButton(label: "Earning Points", icon: Icons.monetization_on, iconBgColor: Colors.purple.shade50),
+                  buildFeatureButton(label: "Voucher", icon: Icons.card_giftcard),
+                  buildFeatureButton(label: "Earning Points", icon: Icons.monetization_on, iconBgColor: Colors.purpleAccent),
                 ]),
 
                 buildCardSection("My Store", [
@@ -108,87 +95,114 @@ class _ProfilePageState extends State<ProfilePage> {
                 ]),
 
                 buildCardSection("Support", [
-                  buildFeatureButton(label: "Contact Us", icon: Icons.support_agent),
+                  buildFeatureButton(label: "Contact Us", icon: Icons.contact_support, iconBgColor: Colors.green),
                 ]),
+
+                SizedBox(height: 80), // for bottom nav bar space
               ],
             ),
           ),
 
         ],
       ),
-      bottomNavigationBar: _buildBottomNavigationBar()
-
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 4,
+        backgroundColor: Colors.grey,
+        onTap: (index) {
+          // handle navigation
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.add_circle_outline), label: 'Post'),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: 'History'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
     );
   }
 
   Widget buildTopSection() {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(20, 40, 20, 15),
-      child: Stack(
+      padding: const EdgeInsets.fromLTRB(20, 60, 20, 15),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Top content
-          Padding(
-            padding: const EdgeInsets.only(top: 20),
-            child: Row(
+          // Bigger Avatar
+          CircleAvatar(
+            radius: 48,
+            backgroundImage: NetworkImage(avatarUrl),
+          ),
+          const SizedBox(width: 15),
+          // User Info Column
+          Expanded(
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // Avatar
-                CircleAvatar(
-                  radius: 48,
-                  backgroundImage: NetworkImage(avatarUrl),
+                // Name + Edit Button Row
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        username,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 100, 0),
+                      child: IconButton(
+                        padding: EdgeInsets.zero,
+                        constraints: BoxConstraints(),
+                        icon: Icon(Icons.edit, size: 18, color: Colors.black54),
+                        onPressed: _navigateToEditPage,
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(width: 15),
 
-                // User Info Column
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Flexible(
-                            child: Text(
-                              username,
-                              style: const TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.edit, size: 18, color: Colors.black54),
-                            padding: const EdgeInsets.only(left: 5),
-                            constraints: const BoxConstraints(),
-                            onPressed: _navigateToEditPage,
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 4),
-
-                      Text(
-                        userId,
-                        style: const TextStyle(fontSize: 15, color: Colors.grey),
-                      ),
-
-
-
-                    ],
+                // Tighter layout between elements
+                const SizedBox(height: 2),
+                Text(
+                  userId,
+                  style: TextStyle(fontSize: 10, color: Colors.blue),
+                ),
+                const SizedBox(height: 4),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlueAccent,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Text(
+                    'Non-Member',
+                    style: TextStyle(color: Colors.white, fontSize: 10),
+                  ),
+                ),
+                const SizedBox(height: 4),
+                TextButton(
+                  onPressed: () {
+                    // go to upgrade page
+                  },
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    minimumSize: Size(0, 0),
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  child: Text(
+                    'Upgrade to become member?',
+                    style: TextStyle(
+                      fontSize: 10,
+                      color: Colors.blueAccent,
+                      decoration: TextDecoration.underline,
+                    ),
                   ),
                 ),
               ],
-            ),
-          ),
-
-          // Settings icon at top-left
-          Positioned(
-            top: 0,
-            right: 0,
-            child: IconButton(
-              icon: const Icon(Icons.settings, color: Colors.black54),
-              onPressed: _navigateToSettingPage,
             ),
           ),
         ],
@@ -196,10 +210,9 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 
-
   Widget buildCardSection(String title, List<Widget> children) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -210,16 +223,16 @@ class _ProfilePageState extends State<ProfilePage> {
               fontWeight: FontWeight.bold,
             ),
           ),
-          const SizedBox(height: 10),
+          const SizedBox(height: 12),
           Container(
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.circular(20),
               boxShadow: [
                 BoxShadow(
-                  color: Colors.black38,
+                  color: Colors.black12,
                   blurRadius: 8,
-                  offset: Offset(4, 4),
+                  offset: Offset(0, 4),
                 ),
               ],
             ),
@@ -247,18 +260,18 @@ class _ProfilePageState extends State<ProfilePage> {
   }) {
     return GestureDetector(
       onTap: onTap,
-
       child: SizedBox(
-
+        width: 130, 
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              padding: EdgeInsets.all(20),
+              padding: EdgeInsets.all(12),
               decoration: BoxDecoration(
                 color: iconBgColor,
                 shape: BoxShape.circle,
               ),
-              child: Icon(icon, color: Colors.black),
+              child: Icon(icon, color: Colors.white),
             ),
             SizedBox(height: 8),
             Text(
@@ -271,93 +284,6 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 
-  Widget _buildBottomNavigationBar() {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey,
-            spreadRadius: 1,
-            blurRadius: 5,
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            _buildNavItem(Icons.home, 'Home',false),
-            _buildNavItem(Icons.shopping_cart, 'Cart', false),
-            _buildAddButton(),
-            _buildNavItem(Icons.history, 'History', false),
-            _buildNavItem(Icons.person, 'Profile', true),
-          ],
-        ),
-      ),
-    );
-  }
 
-  Widget _buildNavItem(IconData icon, String label, bool isSelected) {
-    return Builder(
-      builder: (context) => GestureDetector(
-        onTap: () {
-          switch (label) {
-            case 'Home':
-              Navigator.pushReplacementNamed(context, '/home');
-              break;
-            case 'Cart':
-              Navigator.pushReplacementNamed(context, '/cart');
-              break;
-            case 'History':
-              Navigator.pushReplacementNamed(context, '/history');
-              break;
-            case 'Profile':
-              Navigator.pushReplacementNamed(context, '/profile');
-              break;
-          }
-        },
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(
-              icon,
-              color: isSelected ? Colors.blue : Colors.grey,
-            ),
-            Text(
-              label,
-              style: TextStyle(
-                color: isSelected ? Colors.blue : Colors.grey,
-                fontSize: 12,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildAddButton() {
-    return Container(
-      width: 44,
-      height: 44,
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        shape: BoxShape.circle,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.blue,
-            spreadRadius: 2,
-            blurRadius: 5,
-          ),
-        ],
-      ),
-      child: const Icon(
-        Icons.add,
-        color: Colors.white,
-      ),
-    );
-  }
 
 }
